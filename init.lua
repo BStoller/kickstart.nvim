f809b61d506b0f586adaf8bdc4ab99bbf63dfb82
Blake Stoller <blake@kittypooclub.com>
2025-04-10 15:21:03 -0400

    Auto save

--- init.lua
+++ init.lua
@@ -252,6 +252,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'numToStr/Comment.nvim',
+  'pocco81/auto-save.nvim',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
